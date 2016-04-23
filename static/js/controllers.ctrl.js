8ca920bf62c6e152938a0ba993e3f9c21df78d60
Ryan Bird <dawgillac11@yahoo.com>
2016-04-22 21:55:56 -0700

    first fix

--- static/js/controllers.ctrl.js
+++ static/js/controllers.ctrl.js
@@ -12,7 +12,7 @@ app.controller('HomeController', function($interval, $scope) {
 	console.log("HomeController Loaded")
 
 		var dimages= [];
-		var numImages=13;
+		var numImages=14;
 
 		for (i=0; i<numImages; i++) {
 		  // dimages[i]=new Image();
